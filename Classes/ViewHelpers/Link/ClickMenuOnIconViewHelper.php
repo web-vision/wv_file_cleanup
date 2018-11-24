e77d86181e8a1017e799b6651784f8f9d982aca9
DavidBruchmann <david.bruchmann@gmail.com>
2018-11-25 05:28:49 +0700

    Compatibility to version 8 and 9 of TYPO3: - copy core viewHelpers with compatibility issues to extension - implement 'intelligent' translation handling due to 2x changed location of translation files in core - Adjust template to show display options even with empty result list

---
+++ Classes/ViewHelpers/Link/ClickMenuOnIconViewHelper.php
@@ -0,0 +1,55 @@
+<?php
+namespace WebVision\WvFileCleanup\ViewHelpers\Link;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Class ClickMenuOnIconViewHelper
+ */
+class ClickMenuOnIconViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+{
+    /**
+     * @var string
+     */
+    protected $tagName = 'a';
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerUniversalTagAttributes();
+        $this->registerArgument('table', 'string', 'Table Identifier', false);
+    }
+
+    /**
+     * Renders click menu link (context sensitive menu)
+     *
+     * @param string $table
+     *
+     * @return string
+     * @see \TYPO3\CMS\Backend\Utility\BackendUtility::wrapClickMenuOnIcon()
+     */
+    public function render()
+    {
+        $this->tag->addAttribute('class', 't3-js-clickmenutrigger ' . $this->arguments['class']);
+        $this->tag->addAttribute('data-table', $this->arguments['table']);
+        $this->tag->addAttribute('data-listframe', 1);
+        $this->tag->addAttribute('href', '#');
+
+        $this->tag->setContent($this->renderChildren());
+
+        return $this->tag->render();
+    }
+}
